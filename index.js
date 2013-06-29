f476282c4c6192557bb054d94a0324fa949a1697
Vitaly[AVVS] <v@aminev.me>
2013-06-29 11:32:46 +0400

    setGlobals supported

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 var doT = require('dot'),
     fs  = require('fs'),
-    path = require('path');
+    path = require('path'),
+    _ = require('lodash');
 
 // cache variables
 var _cache = {},
-    _partialsCache = {};
+    _partialsCache = {},
+    _globals = {
+        //tmp function -- disables overwriting via setGlobals
+        load: function(){}
+    };
 
-var partialLoader = function(caller){
-    return {
+var globalGenerator = function(caller){
+    var instance =  {
         load: function (file){
             var template = null;
 
@@ -24,27 +29,54 @@ var partialLoader = function(caller){
             return template;
         }
     };
+    return _.extend({},_globals, instance);
+};
+
+var _settings = doT.templateSettings;
+
+exports.setGlobals = function (globals) {
+    'use strict';
+    for (var f in _globals) {
+        if (globals[f] == null) {
+            globals[f] = _globals[f];
+        }
+        else
+            throw new Error("Your global uses reserved utility: " + f);
+    }
+    _globals = globals;
+};
+
+exports.setTemplateSettings = function (settings) {
+    'use strict';
+    for (var f in settings) {
+        _settings[f] = settings[f];
+    }
 };
 
 function __loadPartial(caller, context){
     return function(file){
 
         // generate loader function
-        var loaderFunc = partialLoader(caller);
+        var globals = globalGenerator(caller);
         // get template, no caching - otherwise will pollute global env
-        var template = doT.template(loaderFunc.load(file), null, loaderFunc);
+        var template = doT.template(globals.load(file), null, globals);
 
-        return template(context);
+        return template.call(globals, context);
     };
 }
 
 
 hexo.extend.renderer.register('dot', 'html', function(data, context){
 
+    var globals = globalGenerator(data.path);
     context.__loadPartial = __loadPartial(data.path, context);
 
-    var template = _cache[data.path] ? _cache[data.path] : doT.template(data.text, null, partialLoader(data.path));
+    // generating template if not exists
+    var template = _cache[data.path] ? _cache[data.path] : doT.template(data.text, null, globals);
+
+    // trying to cache
     _cache[data.path] = template;
-    return template(context);
+
+    return template.call(globals, context);
 
 }, true);
